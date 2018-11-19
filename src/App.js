eff2207123205b34ca35b85be2e9f386d0f96de1
Don Nguyen <don@aerofiler.com>
2018-11-19 15:27:06 +1100

    Firebase

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDSojpbgA5XB-Ahg93tfmUQSt9OX2w9m88',
+      authDomain: 'auth-45f73.firebaseapp.com',
+      databaseURL: 'https://auth-45f73.firebaseio.com',
+      projectId: 'auth-45f73',
+      storageBucket: 'auth-45f73.appspot.com',
+      messagingSenderId: '914690743286'
+    });
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <Text>An app</Text>
+        <LoginForm />
       </View>
     );
   }
